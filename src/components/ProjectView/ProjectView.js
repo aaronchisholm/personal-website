7d160d48291152729c0f84c7e4a9fad4c014a18c
aaronchisholm <84869779+aaronchisholm@users.noreply.github.com>
2024-07-09 15:21:41 -0400

    Updated browserlist

--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -5,7 +5,7 @@ const ProjectView = ({project}) => {
         <div className='container'>
             <h2>{project.title}</h2>
             <h5 className=''>{project.date}</h5>
-            <img className='w-100' src='post.imageSrc'></img>
+            <img className='w-100' src='post.imageSrc' alt='No content yet for this project!'></img>
             <p>{project.body}</p>
         </div>
     )
